5472c553cc76da5af38a21c8cab4ba8389ae29ae
RasecD <malcom_hades@hotmail.com>
2014-10-31 04:27:06 -0600

    Create 1545 - Equidivisions.cpp

---
+++ 1545 - Equidivisions.cpp
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <string.h>
+#include <utility>
+#include <stack>
+using namespace std;
+#define MAX 101
+
+typedef pair <int, int> Coord;
+int square[MAX][MAX];
+bool visitado[MAX][MAX];
+
+void dfs(Coord s, int n)
+{
+	stack <Coord> pila;
+	pila.push(s);	
+	visitado[s.first][s.second] = true;
+	
+	while(!pila.empty())
+	{
+		Coord act = pila.top();
+		pila.pop();
+		
+		if(!visitado[act.first - 1][act.second] && square[act.first - 1][act.second] == square[s.first][s.second] && (act.first - 1) >= 0){
+			visitado[act.first - 1][act.second] = true;
+			pila.push(make_pair(act.first - 1, act.second));
+		}
+		if(!visitado[act.first][act.second - 1] && square[act.first][act.second - 1] == square[s.first][s.second] && (act.second - 1) >= 0){
+			visitado[act.first][act.second - 1] = true;
+			pila.push(make_pair(act.first, act.second - 1));
+		}
+		if(!visitado[act.first + 1][act.second] && square[act.first + 1][act.second] == square[s.first][s.second] && (act.first + 1) < n){
+			visitado[act.first + 1][act.second] = true;
+			pila.push(make_pair(act.first + 1, act.second));
+		}
+		if(!visitado[act.first][act.second + 1] && square[act.first][act.second + 1] == square[s.first][s.second] && (act.second + 1) < n){
+			visitado[act.first][act.second + 1] = true;
+			pila.push(make_pair(act.first, act.second + 1));
+		}
+	}
+}
+
+int dfs_visitar(int n)
+{
+	int numComp = 0;	
+
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			if(!visitado[i][j]){
+				numComp++;
+				dfs(make_pair(i, j), n);
+			}
+		}
+	}
+
+	return numComp;
+}
+
+int main()
+{
+	int n, x, y, total;
+
+	while(1)
+	{
+		scanf("%d", &n);
+		if(n == 0) break;
+		for(int i = 0; i < n - 1; i++){
+			for(int j = 0; j < n; j++){
+				scanf("%d %d", &x, &y);
+				square[x-1][y-1] = i + 1;
+			}
+		}
+		total = dfs_visitar(n);
+		if(total == n)
+			printf("good\n");
+		else
+			printf("wrong\n");
+		memset(square, 0, sizeof(square));
+		memset(visitado, false, sizeof(visitado));
+	}
+
+	return 0;
+}
