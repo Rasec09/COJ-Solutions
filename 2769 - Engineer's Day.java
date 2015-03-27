d5a9e829c4077970158d09453ff3fab7a6282306
RasecD <malcom_hades@hotmail.com>
2015-03-27 02:21:38 -0600

    Create 2769 - Engineer's Day.java

---
+++ 2769 - Engineer's Day.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String [] args){
+		
+		Scanner in = new Scanner(System.in);
+		int N;
+		
+		N = in.nextInt();
+		for(int i = 1; i <= N; i++)
+			System.out.println(i+": I am participating in the Engineer's day.");
+	}
+}
