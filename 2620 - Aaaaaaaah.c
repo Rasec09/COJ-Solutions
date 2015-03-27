c9639e6180b5d4a4102fed26317195f8d6813fca
RasecD <malcom_hades@hotmail.com>
2015-03-27 01:47:19 -0600

    Create 2620 - Aaaaaaaah.c

---
+++ 2620 - Aaaaaaaah.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int r, i;
+
+    scanf("%d", &r);
+    printf("A");
+    for(i = 0; i < 4*r; i++)
+        printf("a");
+    printf("h\n");
+    return 0;
+}
